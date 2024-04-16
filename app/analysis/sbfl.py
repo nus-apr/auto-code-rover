ab112131757612b2d05c950df1a703a8deaa1a9b
crhf <ruanhf8@gmail.com>
2024-04-16 18:21:09 +0800

    Refactor: move SBFL-related stuff to sbfl.py

--- app/analysis/sbfl.py
+++ app/analysis/sbfl.py
@@ -14,12 +14,19 @@ import math
 import os
 import re
 from collections.abc import Mapping
+from configparser import ConfigParser
 from functools import cache
+from os import PathLike
+from os.path import join as pjoin
 from pathlib import Path
 from pprint import pformat
+from subprocess import PIPE, STDOUT, TimeoutExpired
+from tempfile import mkstemp
 
 from coverage.sqldata import CoverageData
 
+import app.utils as apputils
+from app import globals, log
 from app.api.task import PythonTask, Task
 from app.data_structures import MethodId
 
@@ -239,72 +246,300 @@ def run(task: Task) -> tuple[list[str], list[tuple[str, int, float]], str]:
         - path of testing log
     """
     if isinstance(task, PythonTask):
-        return _run_python_task(task)
+        return PythonSbfl.run(task)
 
     raise NotImplementedError(f"SBFL does not support {type(task).__name__}")
 
 
-def _run_python_task(
-    task: PythonTask,  # , cov_file: str
-) -> tuple[list[str], list[tuple[str, int, float]], str]:
-    cov_file, log_file = task.run_developer_test_suite()
+class PythonSbfl:
+    @classmethod
+    def run(
+        cls, task: PythonTask
+    ) -> tuple[list[str], list[tuple[str, int, float]], str]:
+        cov_file, log_file = cls._run_python_developer_test_suite(task)
 
-    pass_tests_names = []
-    fail_tests_names = []
-    test_file_names = []
-    for test in task.testcases_passing:
-        file_name, test_name = canonicalize_testname(task.task_id, test)
-        pass_tests_names.append(test_name)
-        test_file_names.append(file_name)
-    for test in task.testcases_failing:
-        file_name, test_name = canonicalize_testname(task.task_id, test)
-        fail_tests_names.append(test_name)
-        test_file_names.append(file_name)
+        pass_tests_names = []
+        fail_tests_names = []
+        test_file_names = []
+        for test in task.testcases_passing:
+            file_name, test_name = canonicalize_testname(task.task_id, test)
+            pass_tests_names.append(test_name)
+            test_file_names.append(file_name)
+        for test in task.testcases_failing:
+            file_name, test_name = canonicalize_testname(task.task_id, test)
+            fail_tests_names.append(test_name)
+            test_file_names.append(file_name)
 
-    # compute total before removing wierd test names
-    total_fail = len(fail_tests_names)
-    total_pass = len(pass_tests_names)
+        # compute total before removing wierd test names
+        total_fail = len(fail_tests_names)
+        total_pass = len(pass_tests_names)
 
-    pass_tests_names = helper_remove_dup_and_empty(pass_tests_names)
-    fail_tests_names = helper_remove_dup_and_empty(fail_tests_names)
-    test_file_names = helper_remove_dup_and_empty(test_file_names)
+        pass_tests_names = helper_remove_dup_and_empty(pass_tests_names)
+        fail_tests_names = helper_remove_dup_and_empty(fail_tests_names)
+        test_file_names = helper_remove_dup_and_empty(test_file_names)
 
-    if not os.path.isfile(cov_file):
-        raise NoCoverageData(log_file)
+        if not os.path.isfile(cov_file):
+            raise NoCoverageData(log_file)
 
-    covdb = CoverageData(basename=cov_file)
-    covdb.read()
+        covdb = CoverageData(basename=cov_file)
+        covdb.read()
 
-    exec_stats = ExecStats()
+        exec_stats = ExecStats()
 
-    # Collect measured_files
-    measured_files = covdb.measured_files()
-    for measured_f in measured_files:
-        file_exec_stats = FileExecStats(measured_f)
-        current_context = covdb.contexts_by_lineno(measured_f)
-        # not consider files which are not tested
-        if current_context is [] or current_context is None:
-            measured_files.remove(measured_f)
-            continue
-        # store pass/fail stats associated with context and line number
-        for line_no, context_names in current_context.items():
-            for test_name in context_names:
-                if not test_name:  # ''
-                    continue
+        # Collect measured_files
+        measured_files = covdb.measured_files()
+        for measured_f in measured_files:
+            file_exec_stats = FileExecStats(measured_f)
+            current_context = covdb.contexts_by_lineno(measured_f)
+            # not consider files which are not tested
+            if current_context is [] or current_context is None:
+                measured_files.remove(measured_f)
+                continue
+            # store pass/fail stats associated with context and line number
+            for line_no, context_names in current_context.items():
+                for test_name in context_names:
+                    if not test_name:  # ''
+                        continue
 
-                # remove pytest-cov phase name
-                # see https://pytest-cov.readthedocs.io/en/latest/contexts.html#
-                test_name = re.sub(r"\|((setup)|(run)|(teardown))$", "", test_name)
+                    # remove pytest-cov phase name
+                    # see https://pytest-cov.readthedocs.io/en/latest/contexts.html#
+                    test_name = re.sub(r"\|((setup)|(run)|(teardown))$", "", test_name)
 
-                if helper_test_match_any(test_name, pass_tests_names):
-                    file_exec_stats.incre_pass_count(line_no)
-                elif helper_test_match_any(test_name, fail_tests_names):
-                    file_exec_stats.incre_fail_count(line_no)
-        exec_stats.add_file(file_exec_stats)
+                    if helper_test_match_any(test_name, pass_tests_names):
+                        file_exec_stats.incre_pass_count(line_no)
+                    elif helper_test_match_any(test_name, fail_tests_names):
+                        file_exec_stats.incre_fail_count(line_no)
+            exec_stats.add_file(file_exec_stats)
 
-    # NOTE: swap algorithm here
-    ranked_lines = exec_stats.rank_lines(ExecStats.ochiai, total_fail, total_pass)
-    return test_file_names, ranked_lines, log_file
+        # NOTE: swap algorithm here
+        ranked_lines = exec_stats.rank_lines(ExecStats.ochiai, total_fail, total_pass)
+        return test_file_names, ranked_lines, log_file
+
+    @classmethod
+    def _run_python_developer_test_suite(cls, task: PythonTask) -> tuple[str, str]:
+        """
+        Run the relevant parts of developer test suite.
+        Record coverage information for each test while running them.
+
+        Returns:
+            - The produced coverage file for the test suite.
+              Empty string if no coverage file is produced.
+            - Log file.
+        """
+        if "django" in task.task_id:
+            return cls._run_developer_test_suite_django(task)
+        else:
+            return cls._run_developer_test_suite_others(task)
+
+    @classmethod
+    def _run_developer_test_suite_django(cls, task: PythonTask) -> tuple[str, str]:
+        """
+        Since django does not use pytest as the testing framework, we use another procedure.
+
+        Returns:
+            - The produced coverage file for the test suite. Empty string if the file is not produced.
+        """
+
+        tests_dir = pjoin(task.project_path, "tests")
+        assert os.path.isdir(tests_dir)
+
+        execution_dir = tests_dir
+        with apputils.cd(execution_dir):
+            # (1) since we want to use coverage.py with dynamic context, create config first
+            cov_config = pjoin(execution_dir, ".coveragerc")
+            cls._specify_dynamic_context(cov_config)
+
+            # (2) actually run the tests to produce coverage output
+            orig_cmd_parts = task.test_cmd.split(" ")
+            assert (
+                orig_cmd_parts[0] == "./tests/runtests.py"
+            ), f"Test command does not start with ./tests/runtests.py: {task.test_cmd}"
+            test_cmd = (
+                "python -m coverage run "
+                + os.path.basename(orig_cmd_parts[0])
+                + " --parallel 1 "
+                + " ".join(orig_cmd_parts[1:])
+            )
+
+            _, log_file = mkstemp(suffix=".log", prefix="run_developer_tests")
+
+            try:
+                cp = apputils.run_string_cmd_in_conda(
+                    test_cmd,
+                    task.env_name,
+                    stdout=PIPE,
+                    stderr=STDOUT,
+                    text=True,
+                    timeout=globals.test_exec_timeout,
+                )
+                Path(log_file).write_text(cp.stdout)
+            except TimeoutExpired:
+                log.log_and_print(
+                    "Timeout expired while running the test suite.",
+                )
+                return "", log_file
+
+            # (3) check whether the coverage file is there
+            cov_file = pjoin(execution_dir, ".coverage")
+            if not os.path.exists(cov_file):
+                log.log_and_print(
+                    "Coverage file is not produced after running the test suite.",
+                )
+                return "", log_file
+            return cov_file, log_file
+
+    @classmethod
+    def _run_developer_test_suite_others(cls, task: PythonTask) -> tuple[str, str]:
+        """
+        Run the relevant parts of developer test suite.
+        Record coverage information for each test while running them.
+
+        Returns:
+            - The produced coverage file for the test suite. Empty string if no coverage file is produced.
+            - Log file.
+        """
+
+        with apputils.cd(task.project_path):
+            # (1) run the tests to produce coverage output
+            if task.test_cmd.startswith("pytest"):
+                # Use pytest-cov to properly get parametrized test names
+                args = task.test_cmd.removeprefix("pytest")
+                test_cmd = f"python -m pytest --cov --cov-context=test {args}"
+            elif "bin/test" in task.test_cmd:
+                assert task.task_id.startswith("sympy__")
+
+                # Sympy tests are compatible with PyTest. Only issue is that more tests
+                # can berun by PyTest than if Sympy testing is used. However, we match
+                # context names with PASS_TO_PASS and FAIL_TO_PASS later, so it's fine.
+
+                test_files = [x for x in task.test_cmd.split() if x.endswith(".py")]
+                assert (
+                    test_files
+                ), f"Failed to find test files in command: {task.test_cmd}"
+
+                cov_config = pjoin(task.project_path, ".coveragerc")
+
+                cls._omit_coverage_in_file(cov_config, test_files)
+
+                test_cmd = (
+                    "python -m pytest --cov --cov-context=test --no-header"
+                    f" -rA --tb=no -p no:cacheprovider {' '.join(test_files)}"
+                )
+            elif task.test_cmd.startswith("tox "):
+                tox_ini = pjoin(task.project_path, "tox.ini")
+                assert os.path.exists(
+                    tox_ini
+                ), f"tox.ini not found in {task.project_path}"
+
+                cls._add_pytest_cov_to_tox(tox_ini)
+
+                test_cmd = f"python -m {task.test_cmd}"
+            else:
+                cov_config = pjoin(task.project_path, ".coveragerc")
+                cls._specify_dynamic_context(cov_config)
+                test_cmd = f"python -m coverage run {task.test_cmd}"
+
+            _, log_file = mkstemp(suffix=".log", prefix="run_developer_tests")
+            try:
+                cp = apputils.run_string_cmd_in_conda(
+                    test_cmd,
+                    task.env_name,
+                    stdout=PIPE,
+                    stderr=STDOUT,
+                    text=True,
+                    timeout=globals.test_exec_timeout,
+                )
+                Path(log_file).write_text(cp.stdout)
+                # Path(self.output_dir, "run_developer_tests.log").write_text(cp.stdout)
+            except TimeoutExpired:
+                log.log_and_print(
+                    "Timeout expired while running the test suite.",
+                )
+                return "", log_file
+
+            # (2) check whether the coverage file is there
+            cov_file = pjoin(task.project_path, ".coverage")
+            if not os.path.exists(cov_file):
+                # sometimes cov_file can have extensions such as:
+                # .coverage.TSS.852665.XmCvBpdx
+                # we need to find the correct file
+                all_files = os.listdir(task.project_path)
+                for f in all_files:
+                    if f.startswith(".coverage.TSS"):
+                        cov_file = pjoin(task.project_path, f)
+                        break
+                # now check again
+                if not os.path.exists(cov_file):
+                    log.log_and_print(
+                        "Coverage file is not produced after running the test suite.",
+                    )
+                    return "", log_file
+            return cov_file, log_file
+
+    @classmethod
+    def _specify_dynamic_context(cls, coveragerc: str | PathLike) -> None:
+        # check whether there is already a .coveragerc file
+        if not os.path.exists(coveragerc):
+            with open(coveragerc, "w") as f:
+                f.write("[run]\ndynamic_context = test_function")
+        else:
+            # add the dynamic context setting to it
+            with open(coveragerc) as f:
+                lines = f.readlines()
+            updated_lines = []
+            added_context_line = False
+            for line in lines:
+                updated_lines.append(line)
+                if line.startswith("[run]"):
+                    added_context_line = True
+                    updated_lines.append("dynamic_context = test_function\n")
+            # if there is no [run] section in old file, our line
+            # has not been added yet
+            if not added_context_line:
+                updated_lines.append("[run]\n")
+                updated_lines.append("dynamic_context = test_function\n")
+            with open(coveragerc, "w") as f:
+                f.writelines(updated_lines)
+
+    @classmethod
+    def _omit_coverage_in_file(
+        cls, coveragerc: str | PathLike, omitted: list[str]
+    ) -> None:
+        value = "".join(f"\n{file}" for file in omitted)
+        value = "\n# added by auto-code-rover" + value
+
+        config = ConfigParser()
+
+        if os.path.exists(coveragerc):
+            config.read(coveragerc)
+
+        if not config.has_section("run"):
+            config.add_section("run")
+
+        config["run"]["omit"] = value + config["run"].get("omit", "")
+
+        with open(coveragerc, "w") as f:
+            config.write(f)
+
+    @classmethod
+    def _add_pytest_cov_to_tox(cls, tox_ini: str | PathLike):
+        assert os.path.exists(tox_ini)
+
+        config = ConfigParser()
+        config.read(tox_ini)
+
+        assert config.has_section("testenv")
+        config["testenv"]["deps"] = (
+            config["testenv"].get("deps", "") + "\npytest\npytest-cov"
+        )
+
+        assert config.has_option("testenv", "commands")
+        config["testenv"]["commands"] = config["testenv"]["commands"].replace(
+            "pytest", "pytest --cov --cov-context=test"
+        )
+
+        with open(tox_ini, "w") as f:
+            config.write(f)
 
 
 def collate_results(
